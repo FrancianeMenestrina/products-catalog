7f552b48246f07f03fda0d9a0598e59edb1fb0a5
Franciane <francianemenestrina@gmail.com>
2023-07-19 16:26:46 +0200

    Starting Project

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: "n82qji1yn2a6",
+        accessToken: "D3OIrasj6u7X4Kr8Oietel1RUDUo9-EMQEJ0Jj-dABE",
+      },
+    },
+  ],
+};
